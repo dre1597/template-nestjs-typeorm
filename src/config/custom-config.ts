447fa6c0a72935cd12756d72d402a5393332a825
André Pereira <andrepereira97@yahoo.com>
2024-10-04 11:09:29 -0300

    feat: update database configs

--- src/config/custom-config.ts
+++ src/config/custom-config.ts
@@ -21,9 +21,9 @@ const databaseForTests = {
 const database = {
   host: process.env.DATABASE_HOST || 'localhost',
   port: Number(process.env.DATABASE_PORT) || 5432,
-  user: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
-  name: process.env.DATABASE_NAME || 'template',
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  name: process.env.DATABASE_NAME,
   synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
   migrationsRun: process.env.DATABASE_MIGRATIONS_RUN === 'true',
 };
